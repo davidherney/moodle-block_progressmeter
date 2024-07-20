0bfe2111ce3329297a3e138567baea19e8568582
David Herney <davidherney@gmail.com>
2024-07-20 01:42:03 -0500

    Re-compiled js

--- amd/build/main.js
+++
@@ -1,1 +0,0 @@
-define(["jquery"],function(e){M.cfg.wwwroot;var r=function(e,t,s){t<=s&&s<101&&(e.addClass("progressmeter_level-"+t),setTimeout(function(){r(e,t+1,s)},100))};return{init:function(t,s){var n=e("#progressmeter_personal .progressmeter_level"),o=e("#progressmeter_team .progressmeter_level");r(n,1,t),o.length>0&&r(o,1,s)}}});
